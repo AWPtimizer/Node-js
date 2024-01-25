933c1e4c671d113e0a5ee77f0080f4d162501a2c
AWPtimizer <luhemdha@gmail.com>
2024-01-25 21:45:29 +0530

    HTTP Methods/Requests

--- HTTP Methods/index.js
+++ HTTP Methods/index.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const fs = require("fs");
-const url = require("url");
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res; //.end();
+  if (req.url === "/favicon.ico") return res//.end();
   const log = `${Date.now()}: ${req.url} New req received\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
@@ -12,15 +12,8 @@ const myServer = http.createServer((req, res) => {
       case "/":
         res.end("Hello from Server");
         break;
-      case "/signup":
-        if (req.method === "GET") {
-          res.end("This is a signup form");
-        } else if (req.method === "POST") {
-          res.end("Success");
-        }
-        break;
       case "/about":
-        const username = myUrl.query.myname;
+        const username = myUrl.query.myname
         res.end(`Hi, ${username}`);
         break;
       default:
---
+++ HTTP Methods/readme.md
@@ -0,0 +1,16 @@
+### HTTP Methods ->
+    GET
+    POST
+    PUT
+    PATCH
+    DELETE
+
+### GET ->
+    When you want to get some data from the server then you will do GET request.
+
+### POST ->
+    When you want to send and mutate some data in server then you will do POST request.
+    Whenever we fill a form and click on submit, then it will make POST request.
+
+### PUT ->
+    PUT is used to send data to a server to create/update a resource. The difference between POST and PUT is that PUT requests are idempotent. That is, calling the same PUT request multiple times will always produce the same result.
