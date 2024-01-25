871a0473084e68fe1a5d89bad0876ff758c9f799
AWPtimizer <luhemdha@gmail.com>
2024-01-25 10:16:42 +0530

    modules and exports in node js

---
+++ Modules in Node Js/math.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+// another way to export ->
+
+exports.add() = (a, b) => a + b;
+exports.sub() = (a, b) => a - b;
