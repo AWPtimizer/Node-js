6912d965270d40a9e4dcc32157b14812978b55a0
AWPtimizer <luhemdha@gmail.com>
2024-01-26 14:36:59 +0530

    Build HTTP Methods in project-01

--- REST API/project-01/index.js
+++ REST API/project-01/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }));
 
 //Routes ->
 app.get("/users", (req, res) => {
@@ -23,34 +26,13 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-// app.get("/api/users/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   const user = users.find((user) => user.id === id);
-//   return res.json(user);
-// });
-
 app.post("/api/users", (req, res) => {
-  //TODO: Create new user
-  res.json({
-    status: "Pending",
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    res.json({ status: "success", id: users.length });
   });
 });
-
-// app.patch("/api/users/:id", (req, res) => {
-//   //TODO: Edit the user with id
-//   res.json({
-//     status: "Pending",
-//   });
-// });
-
-// app.delete("/api/users/:id", (req, res) => {
-//   //TODO: Delete the user with id
-//   res.json({
-//     status: "Pending",
-//   });
-// });
-
-// As you can see get patch and delete have same path then we can do this below ->
 app
   .route("/api/users/:id")
   .get((req, res) => {
@@ -60,15 +42,33 @@ app
   })
   .patch((req, res) => {
     //TODO: Edit the user with id
-    res.json({
-      status: "Pending",
-    });
+    const id = Number(req.params.id);
+    const updatedUser = req.body;
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      users[index] = { ...users[index], ...updatedUser };
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({
+          status: "Success",
+          msg: "User Updated Successfully",
+        });
+      });
+    }
   })
   .delete((req, res) => {
     //TODO: Delete the user with id
-    res.json({
-      status: "Pending",
-    });
+    const id = Number(req.params.id);
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) {
+      users.splice(index, 1);
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({
+          status: "Success",
+          msg: `Deleted User ${index}`,
+        });
+      });
+    }
   });
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
