e4a50828ec006506bc3f678653325fa80bb13f74
AWPtimizer <luhemdha@gmail.com>
2024-01-26 18:13:37 +0530

    Middlewares

--- REST API/project-01/index.js
+++ REST API/project-01/index.js
@@ -7,6 +7,16 @@ const PORT = 8000;
 
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
 //Routes ->
 app.get("/users", (req, res) => {
   /*
