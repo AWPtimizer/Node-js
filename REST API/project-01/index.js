281c051d29e0fadee53f34c7424237e3a1cac72e
AWPtimizer <luhemdha@gmail.com>
2024-01-26 18:53:59 +0530

    Understanding HTTP Status Codes

--- REST API/project-01/index.js
+++ REST API/project-01/index.js
@@ -43,9 +43,10 @@ app.get("/api/users", (req, res) => {
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (!body || !body.first_name || !body.gender || !body.email || !body.last_name || !body.job_title) return res.status(400).json({msg: "All fields are req..."})
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    res.json({ status: "success", id: users.length });
+    res.status(201).json({ status: "success", id: users.length });
   });
 });
 app
@@ -53,6 +54,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) return res.status(404).json({msg: "User not found!!"})
     return res.json(user);
   })
   .patch((req, res) => {
---
+++ REST API/project-01/readme.md
@@ -0,0 +1,9 @@
+Types of status codes ->
+Informational responses (100 – 199)
+Successful responses (200 – 299)
+Redirection messages (300 – 399)
+Client error responses (400 – 499)
+Server error responses (500 – 599)
+
+It's a good practice to add 201 Status code if ->
+ The request succeeded, and a new resource was created as a result. This is typically the response sent after<b> POST </b>requests, or some <b> PUT </b> requests.
