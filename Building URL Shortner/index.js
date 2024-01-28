930692df3d74c65d6a2ef158c45cc53f8740c921
AWPtimizer <luhemdha@gmail.com>
2024-01-28 10:34:26 +0530

    Node Authentication Statefull

--- Building URL Shortner/index.js
+++ Building URL Shortner/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connection");
 const URL = require("./models/url");
 const path = require("path");
-const staticRoute = require("./routes/staticRouter")
+const cookieParser = require('cookie-parser');
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+const { restrictToLoggedInUserOnly, checkAuth } = require("./middleware/auth");
 
 const app = express();
 const PORT = 8001;
@@ -16,17 +20,12 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/test", async (req, res) => {
-  const allURLs = await URL.find({});
-  return res.render("home", {
-    urls: allURLs,
-  });
-});
-
-app.use("/url", urlRoute);
-app.use("/", staticRoute)
+app.use("/url", restrictToLoggedInUserOnly, urlRoute);
+app.use("/user", checkAuth, userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get("/url/:shorId", async (req, res) => {
   const shortId = req.params.shorId;
